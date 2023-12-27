ee7f6351b3121334272fef55f7512efc76310645
Benyamin <benyaminjmf@gmail.com>
2023-12-27 11:15:32 +0300

    🔐 add CORS approval

--- ai3d/main.py
+++ ai3d/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .endpoints import router
 
@@ -8,6 +9,14 @@ def create_app() -> FastAPI:
     Returns a FastAPI app object.
     """
     app = FastAPI(title="ai3d", openapi_url="/api/openapi.json", version="0.1.0")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allows all origins
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all methods
+        allow_headers=["*"],  # Allows all headers
+    )
 
     app.include_router(router, prefix="/api")
     return app
