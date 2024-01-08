b0e4bf16ef4bb293c84afc0267ab8eb340b6f54a
Benyamin <benyaminjmf@gmail.com>
2024-01-08 13:17:20 +0300

    🤡 mocking hash password

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import client
+from ai3d import utils
 
 
 @pytest_asyncio.fixture()
@@ -11,3 +12,10 @@ async def db() -> AsyncSession:
         await client.create_tables()
         yield session
         await client.drop_tables()
+
+
+# Mocking utils.get_hashed_password function
+def mock_get_hashed_password(password: str) -> str:
+    return "hashed" + password
+
+utils.get_hashed_password = mock_get_hashed_password
